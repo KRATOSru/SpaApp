7d4bb0180880a637374f9b2a997437ddd9a082e7
kratos <vovan198080@inbox.ru>
2020-11-04 20:28:24 +0300

    Toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-<h1>Hello Vovan</h1>
-</template>
+    <v-app id="inspire">
+      <v-navigation-drawer temporary v-model="drawer" app>
+      </v-navigation-drawer>
+
+      <v-app-bar app dark color="primary">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Application</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat>Link one</v-btn>
+        </v-toolbar-items>
+
+      </v-app-bar>
+
+      <v-main>
+        <!--  -->
+      </v-main>
+    </v-app>
+  </template>
+
 
 <script>
   export default {
-    name: "App"
+    name: "App",
+    data: () => ({ drawer: null }),
   }
 </script>
 
