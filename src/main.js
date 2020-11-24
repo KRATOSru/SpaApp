05689944dfe74e1fece10ab7b1d43b0f607bff81
vladimirKartyshov <vovan198080@inbox.ru>
2020-11-24 16:55:53 +0300

    Firebase

--- src/main.js
+++ src/main.js
@@ -3,13 +3,37 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import * as fb from 'firebase'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  components: {App},
+  template: '<App/>',
+  created() {
+      fb.initializeApp({
+          apiKey: 'AIzaSyCpI9T-2zUlqR49-1Pl3huvCi-JDXtW60Y',
+        authDomain: 'spaapp-63602.firebaseapp.com',
+        databaseURL: 'https://spaapp-63602.firebaseio.com',
+        projectId: 'spaapp-63602',
+        storageBucket: 'spaapp-63602.appspot.com',
+        messagingSenderId: '1006982826474',
+        appId: '1:1006982826474:web:e672ece7fb8c16ca3f8a93'
+  })
+  }
+})
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCpI9T-2zUlqR49-1Pl3huvCi-JDXtW60Y",
+//     authDomain: "spaapp-63602.firebaseapp.com",
+//     databaseURL: "https://spaapp-63602.firebaseio.com",
+//     projectId: "spaapp-63602",
+//     storageBucket: "spaapp-63602.appspot.com",
+//     messagingSenderId: "1006982826474",
+//     appId: "1:1006982826474:web:e672ece7fb8c16ca3f8a93"
+// };
+// fb.initializeApp(firebaseConfig);
