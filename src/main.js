9209650977484adcc81ed03d5c989bcfac7a4fa5
vladimirKartyshov <vovan198080"inbox.ru>
2021-02-05 16:10:12 +0300

    corrected some errors

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ new Vue({
     store,
     components: { App },
      created () {
-        fb.initializeApp({
+        var firebaseConfig = {
              apiKey: 'AIzaSyCpI9T-2zUlqR49-1Pl3huvCi-JDXtW60Y',
              authDomain: 'spaapp-63602.firebaseapp.com',
              databaseURL: 'https://spaapp-63602.firebaseio.com',
@@ -50,7 +50,8 @@ new Vue({
              storageBucket: 'spaapp-63602.appspot.com',
              messagingSenderId: '1006982826474',
             appId: '1:1006982826474:web:e672ece7fb8c16ca3f8a93'
-         })
+         }
+         fb.initializeApp(firebaseConfig)
          fb.auth().onAuthStateChanged(user => {
              if (user) {
                  this.$store.dispatch('autoLoginUser', user)
